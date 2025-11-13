3b7d9480df7e96fe472ccbd1a225c6225dc7fb65
Sanskar Gupta <sanskar.gupta@getir.com>
2025-11-13 23:30:14 +0530

    api validations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // // starting point of your application
 // const express = require('express');
-// const { adminAuth } = require('./middlewares/auth'); 
+// const { adminAuth } = require('./middlewares/auth');
 
 // const app = express();
 
@@ -10,7 +10,7 @@
 //     res.send("Hello from test123!!!") ;
 // });
 
-// // universal route for the application, code start matching from top to bottom order matters 
+// // universal route for the application, code start matching from top to bottom order matters
 // app.use("/", (req, res) => {
 //     res.send("Hello from Server!!!");
 // });
@@ -20,7 +20,7 @@
 // });
 
 const express = require("express");
-const connectDB =  require("./config/database"); // this file always run first connect with db then you should listen to requests
+const connectDB = require("./config/database"); // this file always run first connect with db then you should listen to requests
 const User = require("./models/user");
 
 const app = express();
@@ -28,81 +28,107 @@ const app = express();
 app.use(express.json()); // middleware will be actived for all the routes
 
 app.post("/signup", async (req, res) => {
-    console.log(req.body); //data you are sending here but you can't directly ready here, req.body -> undefined because data is sent in json format and our server is not able to read the json data to read that json we need a middle ware
-    // will be used in all the api -> already middleware by express called expressJson Middleware
-    const user = new User(req.body); //creating new instance of user model
+  console.log(req.body); //data you are sending here but you can't directly ready here, req.body -> undefined because data is sent in json format and our server is not able to read the json data to read that json we need a middle ware
+  // will be used in all the api -> already middleware by express called expressJson Middleware
+  const user = new User(req.body); //creating new instance of user model
 
-    try {
-        await user.save(); //data will be save in db this function return a promise
+  try {
+    await user.save(); //data will be save in db this function return a promise
 
-        res.status(200).send("User Added Successfully")
-    } catch (err) {
-        res.status(400).json({
-            error: err.message,
-            code: err.code || "VALIDATION_FAILED",
-            details: err.errors || err
-        });
-    }
-    
+    res.status(200).send("User Added Successfully");
+  } catch (err) {
+    res.status(400).json({
+      error: err.message,
+      code: err.code || "VALIDATION_FAILED",
+      details: err.errors || err,
+    });
+  }
 });
 
 // get user by email
 
-app.get("/user", async(req, res) => {
-    const userEmail = req.body.email;
-    try {
-       const user = await User.findOne({ email: userEmail }).exec();
-       if(user.length === 0) {
-        res.status(200).send("No User Found")
-       }
-       res.send(user);
-    } catch(err) {
-        res.status(400).send( err);
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.findOne({ email: userEmail }).exec();
+    if (user.length === 0) {
+      res.status(200).send("No User Found");
     }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get("/feed", async(req, res) => {
-    try {
-        const users = await User.find({});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
 
-        res.send(users)
-    } catch(err) {
-        res.status(401).send("error found", err);
-    }
+    res.send(users);
+  } catch (err) {
+    res.status(401).send("error found", err);
+  }
 });
 
-app.delete("/deleteUser", async(req, res) => {
-    const userId = req.body.userId;
+app.delete("/deleteUser", async (req, res) => {
+  const userId = req.body.userId;
 
-    try {
-        await User.findByIdAndDelete(userId);
+  try {
+    await User.findByIdAndDelete(userId);
 
-        res.status(200).send("user deleted successfully");
-    } catch(e) {
-        res.status(400).send("Something went wrong")
-    }
+    res.status(200).send("user deleted successfully");
+  } catch (e) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 //update user
-app.patch("/user", async(req, res) => {
-    try {
-        const userId = req.body.userId;
-        const data = req.body;
-        await User.findByIdAndUpdate({ _id: userId }, data, {
-            runValidators: true
-        });
-        res.status(200).send("Updated Successfully");
+app.patch("/user/:userId", async (req, res) => {
+  try {
+    const userId = req.params?.userId;
+    const data = req.body;
+    const ALLOWED_UPDATES = [
+      "firstName",
+      "lastName",
+      "password",
+      "age",
+      "photoUrl",
+      "about",
+      "skills",
+    ];
 
-    } catch(err) {
-        res.status(400).send("User not is Updated" + err.message)
+    const isUpdateAllowed = Object.keys(data).every((k) => {
+      return ALLOWED_UPDATES.includes(k);
+    });
+
+    if (!isUpdateAllowed) {
+      throw new Error("update not allowed");
     }
-})
 
-connectDB().then(() => {
-    console.log("Connected with DB")
-    app.listen(7777, () => {
-        console.log("Server is successfully listening on port 7777");
-    })
-}).catch((err) => {
-    console.error("Database cannot be connected!!");
+    if (data?.skills?.length > 10) {
+      throw new Error("Skills can't have more than 10 skills");
+    }
+
+    const userData = { ...data };
+    const userSkills = new Set(userData?.skills);
+    const sanitzedSkills = { ...data, skills: [...userSkills] };
+
+    await User.findByIdAndUpdate({ _id: userId }, sanitzedSkills, {
+      runValidators: true,
+    });
+    res.status(200).send("Updated Successfully");
+  } catch (err) {
+    res.status(400).send("User not is Updated : " + err.message);
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("Connected with DB");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
